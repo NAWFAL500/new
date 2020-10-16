7f8f7e071b13eb0f68edba6cc78cd6c7e199faab
NAWFAL500 <71877793+NAWFAL500@users.noreply.github.com>
2020-10-16 16:07:28 +0530

    Add files via upload

---
+++ Ground.js
@@ -0,0 +1,28 @@
+class Ground{
+
+  constructor(x,y,width,height){
+
+    var options = {
+      isStatic : true
+
+    }
+
+   this.body = Bodies.rectangle(x,y,width,height,options)
+       World.add(world,this.body)
+     
+       this.width = width
+       this.height = height
+
+  }
+
+    displaya(){
+    var posi = this.body.position
+     rectMode(CENTER)
+     fill("brown")
+     rect(posi.x,posi.y,this.width,this.height)
+     
+   }
+
+
+
+}
